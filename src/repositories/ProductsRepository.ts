b06206424832c362ff3eea0c2fe2dd9e987b3e46
mjperez89 <jmartin.perez@mercadolibre.com>
2024-10-28 13:00:30 -0300

    start with graphics

--- src/repositories/ProductsRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Products } from "../entities/Product";
-
-@EntityRepository(Products)
-class ProductsRepository extends Repository<Products>{ }
-
-export { ProductsRepository };
---
+++ src/services/GraphicsService.ts
@@ -0,0 +1,9 @@
+class GraphicsService {
+    async getGraphics() {
+        return 'Graphics';
+    }
+
+}
+
+export const graphicsService = new GraphicsService()
+export { GraphicsService }
